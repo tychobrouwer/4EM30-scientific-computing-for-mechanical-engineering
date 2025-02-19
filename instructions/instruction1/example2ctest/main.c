f37f0f30a3ad12481a7e1079431ed7597934c541
Tycho Brouwer <tycho.tbrouwer@gmail.com>
2025-02-19 14:45:14 +0100

    fix instruction 1 exercise 2.c

--- instructions/instruction1/example2ctest/main.c
+++ instructions/instruction1/example2ctest/main.c
@@ -78,5 +78,90 @@ int main(void)
 
   reset(coins, n);
 
+  // test 3
+
+  printf("\nTest 3: resetting coin counters\n\n");
+
+  coins[0].count = 3;
+  coins[1].count = 1;
+  coins[2].count = 1;
+  coins[3].count = 1;
+  coins[4].count = 1;
+  coins[5].count = 4;
+
+  reset(coins, n);
+
+  if (coins[0].count == 0 &&
+      coins[1].count == 0 &&
+      coins[2].count == 0 &&
+      coins[3].count == 0 &&
+      coins[4].count == 0 &&
+      coins[5].count == 0)
+  {
+    report(amount, coins, n);
+
+    printf("Test 3 passed\n");
+  }
+  else
+  {
+    report(amount, coins, n);
+
+    printf("Test 3 failed\n");
+  }
+
+  // Test 4
+
+  printf("\nTest 4.1: check valid amount\n\n");
+
+  amount = 100;
+
+  if (check(amount) == 1)
+  {
+    printf("Test 4.1 passed\n");
+  }
+  else
+  {
+    printf("Test 4.1 failed\n");
+  }
+
+  printf("\nTest 4.2: check invalid amount % 5\n\n");
+
+  amount = 6;
+
+  if (check(amount) == 0)
+  {
+    printf("Test 4.2 passed\n");
+  }
+  else
+  {
+    printf("Test 4.2 failed\n");
+  }
+
+  printf("\nTest 4.3: check invalid amount > 1000\n\n");
+
+  amount = 1001;
+
+  if (check(amount) == 0)
+  {
+    printf("Test 4.3 passed\n");
+  }
+  else
+  {
+    printf("Test 4.3 failed\n");
+  }
+
+  printf("\nTest 4.4: check invalid amount < 0\n\n");
+
+  amount = -1;
+
+  if (check(amount) == 0)
+  {
+    printf("Test 4.4 passed\n");
+  }
+  else
+  {
+    printf("Test 4.4 failed\n");
+  }
+
   return 0;
 }
